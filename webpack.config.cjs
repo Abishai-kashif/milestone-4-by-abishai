773f9cb04e083879c788929de13ccb780d9c49e4
Abishai-kashif <abishaikashif@gmail.com>
2024-09-13 05:38:59 +0500

    fixing pach

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     mode: "development",
     devtool: "inline-source-map",
     entry: "./src/script.ts",
-    output: { filename: "bundle.js" },
+    output: { filename: "bundle.js", path: __dirname + "/dist" },
     resolve: { extensions: [".ts", ".js"] },
     module: {
         rules: [{ test: /\.ts/, use: "ts-loader", exclude: /node_modules/ }],
